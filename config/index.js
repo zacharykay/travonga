c5368b1eb91074564ec2b77fe01b60a863dcf818
Zachary Kay <zkay.ca@gmail.com>
2021-09-22 23:46:14 -0700

    prod server changed to netlify

--- config/index.js
+++ config/index.js
@@ -1,6 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 // export const server = dev ? 'http://localhost:3000' : 'https://travonga.com';
-export const server = dev
-	? 'http://localhost:3000'
-	: 'https://github.com/zacharykay/travonga.git';
+export const server = dev ? 'http://localhost:3000' : 'https://travonga.netlify.app';
