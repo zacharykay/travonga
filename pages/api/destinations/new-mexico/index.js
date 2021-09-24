d7a77aabc3eea89c6164268dc4d385c7c4f65299
Zachary Kay <zkay.ca@gmail.com>
2021-09-24 01:50:46 -0700

    fixed minor breaking changes due to typos

--- pages/api/destinations/new-mexico/index.js
+++ pages/api/destinations/new-mexico/index.js
@@ -1,5 +1,5 @@
-// import { attractionGuides } from '../../../../data/attractions/nex-mexico';
-const { attractionGuides } = require('../../../../data/attractions/nex-mexico.json');
+// import { attractionGuides } from '../../../../data/attractions/new-mexico';
+const { attractionGuides } = require('../../../../data/attractions/new-mexico.json');
 
 export default function handler(req, res) {
 	res.status(200).json(attractionGuides);
